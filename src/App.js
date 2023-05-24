87b49976b29f67976217d31dc68d869482b03e0b
ICT ACADEMY <yazid>
2023-05-24 16:54:14 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import { useForm } from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+ 
+
+
+ const { register, handleSubmit } = useForm();
+ const onFormSubmit = data => console.log(data);
+ const onErrors = errors => console.error(errors);
+
+
+
+
+ return (
+  <div style={{textAlign:'center'}}>
+   <Container className="mt-5 pt-5">
+    <h1 className="text-center mb-5">Form</h1>
+<form onSubmit={handleSubmit(onFormSubmit, onErrors)}>
+ <label htmlFor="">NAME</label>
+ <input type="text" name="NAME" {...register('NAME',{ required: "NAME is required" })} id="" /><br></br><br></br>
+ <label htmlFor="">Address</label>
+ <input type="TEXT" name="Address" {...register('Address',{ required: "Address is required" })} id="" /><br></br><br></br>
+ <label htmlFor="">College</label>
+ <input type="TEXT" name="College" {...register('College',{ required: "College is required" })} id="" /><br></br><br></br>
+ <label htmlFor="">Course</label>
+ <input type="TEXT" name="Course" {...register('Course',{ required: "Course is required" })} id="" /><br></br><br></br>
+ <label htmlFor="">SKill</label>
+ <input type="TEXT" name="SKill" {...register('SKill',{ required: "SKill is required" })} id="" /><br></br><br></br>
+
+ <button type="submit">Submit</button>
+</form>
+   </Container>
+  </div>
+ );
 }
 
-export default App;
